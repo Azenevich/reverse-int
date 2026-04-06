4bd9f2199ac19135285e945e9aa65a578a024201
Anastasiya Zenevich <anastzenev@gmail.com>
2026-04-06 23:42:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(number) {
+  const newNumber = Math.abs(number);
+  const arr = newNumber.toString().split('').map(Number);
+  return Number(arr.reverse().toString().replace(/,/g, ''));
 };
